51bf68cb121101ef040a50502d2eb7441499bee9
Ben Madsen <bmadsenonpc@gmail.com>
2025-12-08 13:20:51 -0700

    Get rid of placeholder page

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,12 +1,20 @@
 import { authOptions } from "../api/auth/[...nextauth]/route";
 import { getServerSession } from "next-auth";
 import { redirect } from "next/navigation";
+import { Button } from "@/components/ui/button";
 
 const page = async () => {
   const session = await getServerSession(authOptions);
 
   if (session) {
-    return <div>dashboard</div>;
+    return (
+      <div>
+        <div className="flex flex-row items-center justify-center gap-3">
+          <Button>Pantry</Button>
+          <Button>Generate</Button>
+        </div>
+      </div>
+    );
   } else {
     redirect("/login");
   }
